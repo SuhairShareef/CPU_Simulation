f45ae5ce03fd3a7ef66fd1c4a98115151e74d7d3
SuhairShareef <suhairshareef1999@gmail.com>
2020-05-13 22:41:41 +0300

    Reading input files

--- CPU Simualtion/CPU Simualtion/Source.cpp
+++ CPU Simualtion/CPU Simualtion/Source.cpp
@@ -8,15 +8,40 @@ using namespace std;
 
 int main() {
 
-	//Reading input from file segmentTable.txt
+
 	ifstream fin;
-	fin.open("segmentTable.txt");
+	ofstream fout;
+
+	// Open input from test file folder, to change the file just uncomment
+	// the desired file and comment the previous file
+	fin.open("Test_File/Test_file_1.txt");
+	//fin.open("Test_File/Test_file_1.txt");
+	//fin.open("Test_File/Test_file_1.txt");
+	//fin.open("Test_File/Test_file_1.txt");
+
 
 	/*
-		Reading files from the input files 	
-		
+		Reading file content from the input files
+
+		msize: Physical Memory Size,
+		psize: Page size,
+		Q: Round Robin Quantum,
+		CS:Context Switch,
+		PI: Process Information array that includes four parts:
+			Process ID, Arrival Time, CPU Burst, Size in Bytes.
 	*/
-	do {} while (EOF);
+
+	long int mSize;
+	int pSize, Q, CS;
+	int PI[5][4];
+
+	fin >> mSize >> pSize >> Q >> CS;
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 4; j++)
+			fin >> PI[i][j];
+
+
+
 
 
 	system("pause");
